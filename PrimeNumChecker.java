05dafc64a3d6b7c367878d47a7c51dbe43822671
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-08-01 16:56:00 +0300

    Debug the PrimeNumChecker file and format the code

--- PrimeNumChecker.java
+++ PrimeNumChecker.java
@@ -6,15 +6,29 @@ public class PrimeNumChecker {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=====Prime Number Checker=====");
+        int number = -1;
         
         while (true) {
-            boolean isPrime = true;
+        
             System.out.println("Enter a positive integer: ");
             
             try {
-                int number = scanner.nextInt();
+                number = scanner.nextInt();
 
-                if (number <= 1) {
+                if (number > 0) {
+                    break;
+                }else{
+                    System.out.println("Please enter a number greater than 0.");
+                }
+   
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter a valid positive integer.");
+                scanner.next();
+            }
+        }
+
+        boolean isPrime = true;
+        if (number <= 1) {
                     isPrime = false;
                 }else{
                     for(int i = 2;i <= number/2;i++){
@@ -30,15 +44,8 @@ public class PrimeNumChecker {
                     System.out.println("The number is prime.");
                 }else{
                     System.out.println("The number is not prime.");
-                    break;
                 }
 
-                
-            } catch (Exception e) {
-                System.out.println("Invalid input! Please enter a valid positive integer.");
-                scanner.next();
-            }   
-        }
 
         scanner.close();    
         
